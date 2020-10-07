ca229edf860d0e23ed9f57e24ded3e0e280e182d
Gonblo <72457007+Gonblo@users.noreply.github.com>
2020-10-07 17:14:00 +0200

    Update AutoDHCP.sh

--- AutoDHCP.sh
+++ AutoDHCP.sh
@@ -83,7 +83,117 @@ echo "# On what interfaces should the DHCP server (dhcpd) serve DHCP requests?" 
 echo "#       Separate multiple interfaces with spaces, e.g. "eth0 eth1"." >> /etc/default/isc-dhcp-server
 echo "INTERFACES="enp0s8"" >> /etc/default/isc-dhcp-server
 
-
+echo "#" >> /etc/dhcp/dhcpd.conf
+echo "# Sample configuration file for ISC dhcpd for Debian" >> /etc/dhcp/dhcpd.conf
+echo "#" >> /etc/dhcp/dhcpd.conf
+echo "# Attention: If /etc/ltsp/dhcpd.conf exists, that will be used as" >> /etc/dhcp/dhcpd.conf
+echo "# configuration file instead of this file." >> /etc/dhcp/dhcpd.conf
+echo "#" >> /etc/dhcp/dhcpd.conf
+echo "#" >> /etc/dhcp/dhcpd.conf
+echo "" >> /etc/dhcp/dhcpd.conf
+echo "# The ddns-updates-style parameter controls whether or not the server will" >> /etc/dhcp/dhcpd.conf
+echo "# attempt to do a DNS update when a lease is confirmed. We default to the" >> /etc/dhcp/dhcpd.conf
+echo "# behavior of the version 2 packages ('none', since DHCP v2 didn't" >> /etc/dhcp/dhcpd.conf
+echo "# have support for DDNS.)" >> /etc/dhcp/dhcpd.conf
+echo "ddns-update-style none;" >> /etc/dhcp/dhcpd.conf
+echo "" >> /etc/dhcp/dhcpd.conf
+echo "# option definitions common to all supported networks..." >> /etc/dhcp/dhcpd.conf
+echo "option domain-name "example.org";" >> /etc/dhcp/dhcpd.conf
+echo "option domain-name-servers ns1.example.org, ns2.example.org;" >> /etc/dhcp/dhcpd.conf
+echo "" >> /etc/dhcp/dhcpd.conf
+echo "default-lease-time 600;" >> /etc/dhcp/dhcpd.conf
+echo "max-lease-time 7200;" >> /etc/dhcp/dhcpd.conf
+echo "" >> /etc/dhcp/dhcpd.conf
+echo "# If this DHCP server is the official DHCP server for the local" >> /etc/dhcp/dhcpd.conf
+echo "# network, the authoritative directive should be uncommented." >> /etc/dhcp/dhcpd.conf
+echo "#authoritative;" >> /etc/dhcp/dhcpd.conf
+echo "" >> /etc/dhcp/dhcpd.conf
+echo "# Use this to send dhcp log messages to a different log file (you also" >> /etc/dhcp/dhcpd.conf
+echo "# have to hack syslog.conf to complete the redirection)." >> /etc/dhcp/dhcpd.conf
+echo "log-facility local7;" >> /etc/dhcp/dhcpd.conf
+echo "" >> /etc/dhcp/dhcpd.conf
+echo "# No service will be given on this subnet, but declaring it helps the" >> /etc/dhcp/dhcpd.conf
+echo "# DHCP server to understand the network topology." >> /etc/dhcp/dhcpd.conf
+echo "" >> /etc/dhcp/dhcpd.conf
+echo "#subnet 10.152.187.0 netmask 255.255.255.0 {" >> /etc/dhcp/dhcpd.conf
+echo "#}" >> /etc/dhcp/dhcpd.conf
+echo "" >> /etc/dhcp/dhcpd.conf
+echo "# This is a very basic subnet declaration." >> /etc/dhcp/dhcpd.conf
+echo "" >> /etc/dhcp/dhcpd.conf
+echo "#subnet 10.254.239.0 netmask 255.255.255.224 {" >> /etc/dhcp/dhcpd.conf
+echo "#  range 10.254.239.10 10.254.239.20;" >> /etc/dhcp/dhcpd.conf
+echo "#  option routers rtr-239-0-1.example.org, rtr-239-0-2.example.org;" >> /etc/dhcp/dhcpd.conf
+echo "#}" >> /etc/dhcp/dhcpd.conf
+echo "" >> /etc/dhcp/dhcpd.conf
+echo "# This declaration allows BOOTP clients to get dynamic addresses," >> /etc/dhcp/dhcpd.conf
+echo "# which we don't really recommend." >> /etc/dhcp/dhcpd.conf
+echo "" >> /etc/dhcp/dhcpd.conf
+echo "#subnet 10.254.239.32 netmask 255.255.255.224 {" >> /etc/dhcp/dhcpd.conf
+echo "#  range dynamic-bootp 10.254.239.40 10.254.239.60;" >> /etc/dhcp/dhcpd.conf
+echo "#  option broadcast-address 10.254.239.31;" >> /etc/dhcp/dhcpd.conf
+echo "#  option routers rtr-239-32-1.example.org;" >> /etc/dhcp/dhcpd.conf
+echo "#}" >> /etc/dhcp/dhcpd.conf
+echo "" >> /etc/dhcp/dhcpd.conf
+echo "# A slightly different configuration for an internal subnet." >> /etc/dhcp/dhcpd.conf
+echo "#subnet 10.5.5.0 netmask 255.255.255.224 {" >> /etc/dhcp/dhcpd.conf
+echo "#  range 10.5.5.26 10.5.5.30;" >> /etc/dhcp/dhcpd.conf
+echo "#  option domain-name-servers ns1.internal.example.org;" >> /etc/dhcp/dhcpd.conf
+echo "#  option domain-name "internal.example.org";" >> /etc/dhcp/dhcpd.conf
+echo "#  option subnet-mask 255.255.255.224;" >> /etc/dhcp/dhcpd.conf
+echo "#  option routers 10.5.5.1;" >> /etc/dhcp/dhcpd.conf
+echo "#  option broadcast-address 10.5.5.31;" >> /etc/dhcp/dhcpd.conf
+echo "#  default-lease-time 600;" >> /etc/dhcp/dhcpd.conf
+echo "#  max-lease-time 7200;" >> /etc/dhcp/dhcpd.conf
+echo "#}" >> /etc/dhcp/dhcpd.conf
+echo "" >> /etc/dhcp/dhcpd.conf
+echo "# Hosts which require special configuration options can be listed in" >> /etc/dhcp/dhcpd.conf
+echo "# host statements.   If no address is specified, the address will be" >> /etc/dhcp/dhcpd.conf
+echo "# allocated dynamically (if possible), but the host-specific information" >> /etc/dhcp/dhcpd.conf
+echo "# will still come from the host declaration." >> /etc/dhcp/dhcpd.conf
+echo "" >> /etc/dhcp/dhcpd.conf
+echo "#host passacaglia {" >> /etc/dhcp/dhcpd.conf
+echo "#  hardware ethernet 0:0:c0:5d:bd:95;" >> /etc/dhcp/dhcpd.conf
+echo "#  filename "vmunix.passacaglia";" >> /etc/dhcp/dhcpd.conf
+echo "#  server-name "toccata.fugue.com";" >> /etc/dhcp/dhcpd.conf
+echo "#}" >> /etc/dhcp/dhcpd.conf
+echo "" >> /etc/dhcp/dhcpd.conf
+echo "# Fixed IP addresses can also be specified for hosts.   These addresses" >> /etc/dhcp/dhcpd.conf
+echo "# should not also be listed as being available for dynamic assignment." >> /etc/dhcp/dhcpd.conf
+echo "# Hosts for which fixed IP addresses have been specified can boot using" >> /etc/dhcp/dhcpd.conf
+echo "# BOOTP or DHCP.   Hosts for which no fixed address is specified can only" >> /etc/dhcp/dhcpd.conf
+echo "# be booted with DHCP, unless there is an address range on the subnet" >> /etc/dhcp/dhcpd.conf
+echo "# to which a BOOTP client is connected which has the dynamic-bootp flag" >> /etc/dhcp/dhcpd.conf
+echo "# set." >> /etc/dhcp/dhcpd.conf
+echo "#host fantasia {" >> /etc/dhcp/dhcpd.conf
+echo "#  hardware ethernet 08:00:07:26:c0:a5;" >> /etc/dhcp/dhcpd.conf
+echo "#  fixed-address fantasia.fugue.com;" >> /etc/dhcp/dhcpd.conf
+echo "#}" >> /etc/dhcp/dhcpd.conf
+echo "" >> /etc/dhcp/dhcpd.conf
+echo "# You can declare a class of clients and then do address allocation" >> /etc/dhcp/dhcpd.conf
+echo "# based on that.   The example below shows a case where all clients" >> /etc/dhcp/dhcpd.conf
+echo "# in a certain class get addresses on the 10.17.224/24 subnet, and all" >> /etc/dhcp/dhcpd.conf
+echo "# other clients get addresses on the 10.0.29/24 subnet." >> /etc/dhcp/dhcpd.conf
+echo "" >> /etc/dhcp/dhcpd.conf
+echo "#class "foo" {" >> /etc/dhcp/dhcpd.conf
+echo "#  match if substring (option vendor-class-identifier, 0, 4) = "SUNW";" >> /etc/dhcp/dhcpd.conf
+echo "#}" >> /etc/dhcp/dhcpd.conf
+echo "" >> /etc/dhcp/dhcpd.conf
+echo "#shared-network 224-29 {" >> /etc/dhcp/dhcpd.conf
+echo "#  subnet 10.17.224.0 netmask 255.255.255.0 {" >> /etc/dhcp/dhcpd.conf
+echo "#    option routers rtr-224.example.org;" >> /etc/dhcp/dhcpd.conf
+echo "#  }" >> /etc/dhcp/dhcpd.conf
+echo "#  subnet 10.0.29.0 netmask 255.255.255.0 {" >> /etc/dhcp/dhcpd.conf
+echo "#    option routers rtr-29.example.org;" >> /etc/dhcp/dhcpd.conf
+echo "#  }" >> /etc/dhcp/dhcpd.conf
+echo "#  pool {" >> /etc/dhcp/dhcpd.conf
+echo "#    allow members of "foo";" >> /etc/dhcp/dhcpd.conf
+echo "#    range 10.17.224.10 10.17.224.250;" >> /etc/dhcp/dhcpd.conf
+echo "#  }" >> /etc/dhcp/dhcpd.conf
+echo "#  pool {" >> /etc/dhcp/dhcpd.conf
+echo "#    deny members of "foo";" >> /etc/dhcp/dhcpd.conf
+echo "#    range 10.0.29.10 10.0.29.230;" >> /etc/dhcp/dhcpd.conf
+echo "#  }" >> /etc/dhcp/dhcpd.conf
+echo "#}" >> /etc/dhcp/dhcpd.conf
 fi
 fi 
 
